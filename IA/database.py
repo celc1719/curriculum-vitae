fc81699a5652327e496f06a0fab434f8f24adf47
celc1719 <celc1719@gmail.com>
2026-07-22 22:40:39 -0600

    Delete IA directory

--- IA/database.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-def get_database():
-    uri = "mongodb+srv://yarvis:Rodrigo1234@ro.84evfrp.mongodb.net/?retryWrites=true&w=majority&appName=ro"
-    client = MongoClient(uri)
-    return client["MiDG"]
